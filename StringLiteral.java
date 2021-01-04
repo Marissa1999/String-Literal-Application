b07b908d1c362dff60e77c4a567fba42beb05666
Marissa Gonçalves <marissagoncalves13@gmail.com>
2021-01-04 17:46:52 -0500

    Initial Commit.

---
+++ StringLiteral.java
@@ -0,0 +1,23 @@
+//Programmer: Marissa Gonçalves
+//Date: August 25, 2017
+//Purpose: This program must display four lines of text twice, using four System.out.println statements and one System.out.print statement.
+
+public class StringLiteral
+
+{
+	public static void main (String[] args)
+
+	{
+		//print a message twice using the guidelines from the purpose;
+		System.out.println (" In Java, a string literal is a sequence of characters inside ");
+		System.out.println (" a pair of double quaotation marks, such as \"Java is fun\". ");
+		System.out.println (" Java programmers should understand the difference between \" ");
+		System.out.println (" and \\\" as well as the difference between \' and \". ");
+		System.out.println ();
+		System.out.print (" In Java, a string literal is a sequence of characters inside\n" +
+		                  " a pair of double quaotation marks, such as \"Java is fun\".\n" +
+		                  " Java programmers should understand the difference between \"\n" +
+		                  " and \\\" as well as the difference between \' and \".\n");
+    }
+
+}
